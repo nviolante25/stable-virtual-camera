[FIX] Dataloader intrinsic scaling, removed latents scale to be handled in the DiffusionEngine first_stage_model. TODO: bugfix samples.

main.py
@@ -22,9 +22,9 @@ from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.trainer import Trainer
 from pytorch_lightning.utilities import rank_zero_only
-
+from diffusers import AutoencoderKL
 from sgm.util import exists, instantiate_from_config, isheatmap
-from seva.modules.autoencoder import AutoEncoder
+
 import threading
 import queue
 from typing import Dict
@@ -322,6 +322,7 @@ class LogTask:
     current_epoch: int
     batch_idx: int
     pl_module: pl.LightningModule
+    scale_factor: float
 
 class ImageLogger(Callback):
     def __init__(
@@ -354,7 +355,14 @@ class ImageLogger(Callback):
         self.log_before_first_step = log_before_first_step
 
         # for logging
-        self.cpu_decoder = None
+        with torch.device("cpu"):
+            self.cpu_decoder = AutoencoderKL.from_pretrained(
+                    "stabilityai/stable-diffusion-2-1-base",
+                    subfolder="vae",
+                    force_download=False,
+                    low_cpu_mem_usage=False,
+                ).eval()
+        self.cpu_decoder.requires_grad_(False)
         self.log_queue = queue.Queue()
         self.log_thread = None
         self.shutdown_event = threading.Event()
@@ -371,8 +379,15 @@ class ImageLogger(Callback):
         print("[ImageLogger] Log worker started")
         
         # Initialize CPU decoder for the worker thread
-        with torch.device("cpu"):
-            self.cpu_decoder = AutoEncoder().eval()
+        # with torch.device("cpu"):
+        #     self.cpu_decoder = AutoencoderKL.from_pretrained(
+        #         "stabilityai/stable-diffusion-2-1-base",
+        #         subfolder="vae",
+        #         force_download=False,
+        #         low_cpu_mem_usage=False,
+        #     ).eval()
+        #     self.cpu_decoder.requires_grad_(False)
+            
         
         while not self.shutdown_event.is_set():
             try:
@@ -421,7 +436,11 @@ class ImageLogger(Callback):
                 if k == "inputs":
                     continue  # Already in RGB space
                 if isinstance(images[k], torch.Tensor):
-                    images[k] = self.cpu_decoder.decode(images[k])
+                    # assuming we use AutoencoderKL
+                    if isinstance(self.cpu_decoder, AutoencoderKL):
+                        images[k] = self.cpu_decoder.decode(images[k] / task.scale_factor).sample
+                    else:
+                        raise Exception("Assumed AutoencoderKL as the cpu_decoder during logging images!")
                     images[k] = self._convert_valid_log_format(images[k], images["inputs"])
                     if self.clamp and not isheatmap(images[k]):
                         images[k] = torch.clamp(images[k], -1.0, 1.0)
@@ -449,7 +468,8 @@ class ImageLogger(Callback):
             global_step=global_step,
             current_epoch=current_epoch,
             batch_idx=batch_idx,
-            pl_module=pl_module
+            pl_module=pl_module,
+            scale_factor=pl_module.scale_factor
         )
         
         try:

seva/data/mvh_dataloader.py
@@ -7,7 +7,6 @@ from tqdm import tqdm
 from typing import Tuple, Optional, Dict, Union, Callable
 
 from seva.geometry import get_plucker_coordinates
-from seva.modules.autoencoder import AutoEncoder
 from sgm.data.read_write_model import read_model
 from sgm.data.utils_camera import (
     read_intrinsics_colmap,
@@ -494,10 +493,11 @@ class MVHumanNetDataset(Dataset):
         # get camera parameters
         extrinsics = self.cam_params[subject_id]['extrinsics']
         intrinsics = np.array(self.cam_params[subject_id]['intrinsics'])
-        camera_scale = self.cam_params[subject_id]['camera_scale']        
+        camera_scale = self.cam_params[subject_id]['camera_scale'] 
 
-        if self.pre_scale != 1: # update intrinsics (for MVHumanNet)
+        if self.pre_scale != 1: # update intrinsics (for MVHumanNet default 0.5x prescaling)
             intrinsics = update_intrinsics_resize(intrinsics, scale=self.pre_scale)
+
 
         # Sample frames indices
         camera_order = []
@@ -541,9 +541,6 @@ class MVHumanNetDataset(Dataset):
 
             latent_tensors = [npz_data[f"{sample_cam}.{timestep}"] for sample_cam in camera_order]
             clean_latents = torch.stack([torch.from_numpy(latent_tensor) for latent_tensor in latent_tensors])
-            if self.apply_scale_factor: # if precomputed latents are NOT scaled
-                clean_latents = clean_latents * self.scale_factor
-            # ! on precomputed latents, forgot to scale by scale_factor, so we do it here
             # (batch_size, 4, 72, 72)
         else:
             # currently, we REQUIRE precomputed latents, so we throw error
@@ -575,23 +572,14 @@ class MVHumanNetDataset(Dataset):
 
         camera_mask = torch.ones(self.num_images, dtype=torch.bool)
 
-
         def get_c2w(cam):
             tf_matrix = create_transform_matrix(
-                np.array(extrinsics[cam]['rotation']).T,  # Transpose R for w2c -> c2w
-                -np.array(extrinsics[cam]['rotation']).T @ (np.array(extrinsics[cam]['translation']) * camera_scale),
+                np.array(extrinsics[cam]['rotation']),
+                np.array(extrinsics[cam]['translation']) * camera_scale,
                 homogeneous=True
             )
 
-            rotation_z_180 = np.array([
-                [1, 0, 0, 0],
-                [0, -1, 0, 0],
-                [0, 0, -1, 0],
-                [0, 0, 0, 1]
-            ])
-        
-            # Apply the rotation to the transform matrix (for MVHN-expected coordinate frame)
-            return rotation_z_180 @ tf_matrix @ rotation_z_180
+            return np.linalg.inv(tf_matrix) # w2c -> c2w
 
         # Read extrinsics (w2c -> c2w)
         all_c2ws = np.array([
@@ -603,23 +591,15 @@ class MVHumanNetDataset(Dataset):
         center_cameras(all_c2ws, c2ws)  # mean center
         scale_cameras(c2ws)
 
-        # print("all_c2ws.shape: ", all_c2ws.shape)
-        # print("c2ws.shape: ", c2ws.shape)
-
         # create intrinsics tensor, update intrinsics
         # TODO: accept multiple camera intrinsics (for random cropping)
         # NOTE: for preliminary fine-tune testing, we currently account for the uniform center crop
-        crop_amount = (self.image_shape[1] - self.target_shape[0]) // 2 # (W - H) // 2: assumes W > H
-        Ks = update_intrinsics(np.array(intrinsics), crop_x=crop_amount, crop_y=0, scale=1)
-        # print("Ks [before norm]: ", Ks)
+        crop_amount  = (self.image_shape[1] - self.target_shape[0]) // 2 # (W - H) // 2: assumes W > H
+        scale_amount = (self.target_shape[0] / self.image_shape[0])
+        Ks = update_intrinsics(np.array(intrinsics), crop_x=crop_amount, crop_y=0, scale=scale_amount)
         Ks = normalize_intrinsics(Ks, self.image_shape[0], self.image_shape[1]) # normalize intrinsics (H,W)
-        # print("Ks [after norm]: ", Ks)
         Ks = repeat(Ks, 'd1 d2 -> n d1 d2', n=self.num_images) # assumes all intrinsics are the same for now 
         Ks = torch.from_numpy(Ks).float()
-
-
-        # print("Ks.shape: ", Ks.shape)
-        # print("Ks: ", Ks.max(), Ks.min())
 
         w2cs = torch.linalg.inv(c2ws)
         pluckers = get_plucker_coordinates(
@@ -649,7 +629,7 @@ class MVHumanNetDataset(Dataset):
 
         replace = torch.cat( # clean latents and binary mask
             [
-                clean_latents, # * self.scale_factor,
+                clean_latents * self.scale_factor,
                 repeat(
                     input_frames_mask,
                     "n -> n 1 h w",

seva/data/preprocessing.py
@@ -13,6 +13,7 @@ from copy import deepcopy
 from glob import glob
 import pickle
 import torch
+from math import isclose
 
 def load_pickle(file_path):
     with open(file_path, 'rb') as f:
@@ -246,15 +247,15 @@ def get_mvhumannet_extrinsics(extrinsics_dict, scale):
     extrinsics[:3, 3] = extrinsics[:3, 3] * scale
     return extrinsics
 
-def update_intrinsics(K, crop_x=0, crop_y=0, scale=1, crop_first=True, padding_mode=False):
+def update_intrinsics(K, crop_x=0, crop_y=0, scale=1.0, crop_first=True, padding_mode=False):
     """Update intrinsic matrix for the crop and resizes."""
     K_new = K.copy() if type(K) == np.ndarray else K.clone()
     if crop_first:
         K_new = update_intrinsics_crop(K, crop_x, crop_y, padding_mode)
-        if scale != 1:
+        if not isclose(scale, 1):
             K_new = update_intrinsics_resize(K_new, scale)
     else:
-        if scale != 1:
+        if not isclose(scale, 1):
             K_new = update_intrinsics_resize(K, scale)
         K_new = update_intrinsics_crop(K_new, crop_x, crop_y, padding_mode)
     return K_new

sgm/models/diffusion.py
@@ -15,8 +15,6 @@ from ..modules.ema import LitEma
 from ..util import (default, disabled_train, get_obj_from_str,
                     instantiate_from_config, log_txt_as_img)
 
-from autoenc import AutoEncoder
-
 def compute_psnr(pred, target):
     mse = torch.nn.functional.mse_loss(pred, target)
     return 10 * torch.log10(1.0 / mse)
@@ -122,7 +120,7 @@ class DiffusionEngine(pl.LightningModule):
 
     @torch.no_grad()
     def decode_first_stage(self, z):
-        # z = 1.0 / self.scale_factor * z
+        z = 1.0 / self.scale_factor * z
         n_samples = default(self.en_and_decode_n_samples_a_time, z.shape[0])
 
         n_rounds = math.ceil(z.shape[0] / n_samples)
@@ -153,7 +151,7 @@ class DiffusionEngine(pl.LightningModule):
                 )
                 all_out.append(out)
         z = torch.cat(all_out, dim=0)
-        # z = self.scale_factor * z
+        z = self.scale_factor * z
         return z
 
     def forward(self, x, batch):